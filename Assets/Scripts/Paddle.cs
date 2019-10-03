f26c4bcfaac9cc7454c9e5c518f25e64c4059faf
Situ William <situ_william@yahoo.com>
2019-10-02 23:55:12 -0400

    feat: added functional paddle

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    // Update is called once per frame
+    void Update()
+    {
+        // your position needs to be proportional to the width of the screen
+        // Input.mousePosition.x / Screen.width
+        // This will be a percentage unit from 1 to -1
+        // After multiplying by width it will be 1 to 16
+        float mouseX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        // Vector2 is a compact way of storing x and y coordinates
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseX, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        // Scene index 0 will always be the start screen
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
